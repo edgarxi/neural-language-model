cab7954981d277c323db443fa167fd835c027136
edgarwest <esx@andrew.cmu.edu>
2017-11-17 22:44:07 -0500

    screw things up by switching all dimensions

--- NLM.py
+++ NLM.py
@@ -11,10 +11,10 @@ class NLM(object):
 		a1 =  (6.0/(vocabSize+dim))**0.5
 		self.C = np.random.uniform(-a1, a1, (vocabSize, dim))
 		# need a word vector representer
-		self.H = np.random.uniform(-a1, a1, (nhidden,nwords*dim)) #(128x48)
-		self.bias1 = np.random.uniform(-a1, a1, (nhidden,1)) #128xbatch size	
+		self.H = np.random.uniform(-a1, a1, (nwords*dim,nhidden,)) #(48x128)
+		self.bias1 = np.random.uniform(-a1, a1, (1,nhidden)) #128xbatch size	
 		self.U = np.random.uniform(-a1, a1, (vocabSize, nhidden))
-		self.bias2 = np.random.uniform(-a1, a1, (vocabSize,1))
+		self.bias2 = np.random.uniform(-a1, a1, (1, vocabSize))
 		self.dim = dim
 	def forwardProp(self, X):
 		O = np.dot(self.H, X.T)+self.bias1 #these are correct i think #128 x bsize
@@ -30,6 +30,7 @@ class NLM(object):
 		T  = X[:,3]
 
 		T = to_onehot(T, len(X))
+		print len(T)
 		c1 = self.C[w1] #continous representation of the word. 
 		c2 = self.C[w2] #dimensions: batch_size x depth 
 		c3 = self.C[w3]
@@ -59,28 +60,26 @@ class NLM(object):
 			w1 = X[:,0] # batch_size x 1
 			w2 = X[:,1] 
 			w3 = X[:,2]
-			W_t = X[:,:2]
 
 			T  = X[:,3] #8,000 x 20 ?
-			T = to_onehot(T, len(X)) # is oneHot wrong?
+
+			T = to_onehot(T, len(X)) # the onehot has COLUMNS. ()8,000 x 20
+			#print np.shape(T)
 			c1 = self.C[w1] #continous representation of the word. 
 			c2 = self.C[w2] #dimensions: batch_size x depth 
 			c3 = self.C[w3]
 			#print np.shape(c3)
 			#T =  self.C[w4]
 			e = np.concatenate((c1, np.concatenate((c2, c3), axis=1)), axis=1) #batch_size x 3D generated 
-			print np.shape(e)
+			print np.shape(e), np.shape(self.H)
 			# ____________________________________forward propogation step__________________________________
-			O = np.dot(self.H, e.T)+self.bias1 #these are correct i think
+			O = np.dot(e, self.H)+self.bias1 #these are correct i think
 			#A = np.tanh(O) # tanh layer
-			B = np.dot(self.U, O)+self.bias2
+			B = np.dot(O,self.U)+self.bias2
 			Y = utility.softmax(B) # 8,000 by 20
-
 			#Y = self.forwardProp(e)
-
 			perplexity = utility.crossEntropy(Y, T)/batchsize
 			print "perplexity: {}".format(perplexity)
-
 			# _____________________________________backpropagation step __________________________________
 			partial_B = (Y-T) # 8,000  x 20 
 			#print np.shape(partial_B)
@@ -96,15 +95,16 @@ class NLM(object):
 			#print np.shape(partial_b2)
 			partial_O =  np.dot(self.U.T, partial_B) # (128xbatch) 
 			assert np.shape(partial_O) == np.shape(O)
-			partial_H = np.dot(partial_O, e)
+			partial_H = np.dot(partial_O, e.T)
 			assert np.shape(partial_H) == np.shape(self.H)
 			#print np.shape(partial_H)
 			#print np.shape(self.H), np.shape(partial_O)
 			partial_X = np.dot(self.H.T, partial_O)  #????????
 
-			print np.shape(partial_X), np.shape(e)
-			assert np.shape(partial_X) == np.shape(T)
+			#print np.shape(partial_X), np.shape(e)
+			assert np.shape(partial_X) == np.shape(e)
 			partial_b1 = np.sum(partial_O, axis=1).reshape(-1, 1)
+			assert np.shape(partial_b1)== np.shape(self.bias1)
 			#print np.shape(partial_b1)
 			#print np.shape(self.bias1)
 
@@ -115,9 +115,10 @@ class NLM(object):
 			self.bias2 -= lr* partial_b2# / batchsize
 			#print "one:" , np.shape(self.C[w1])
 			#print np.shape(partial_X[:self.dim])
-			self.C[w1] -= lr*partial_X[:,:self.dim] 
-			self.C[w2] -= lr*partial_X[:,self.dim:2*self.dim] 
-			self.C[w3] -= lr*partial_X[:,2*self.dim:3*self.dim] 
+			#print np.shape(self.C[w1]), np.shape(partial_X[:,self.dim])
+			#self.C[w1] -= lr*partial_X[:self.dim].T/ batchsize
+			#self.C[w2] -= lr*partial_X[self.dim:self.dim*2].T / batchsize#[:,self.dim:2*self.dim]
+			#self.C[w3] -= lr*partial_X[self.dim*2:self.dim*3].T / batchsize#[:,2*self.dim:3*self.dim]
 
 X = utility.parse("train-new.txt")
 Val = utility.parse("val-new.txt")
